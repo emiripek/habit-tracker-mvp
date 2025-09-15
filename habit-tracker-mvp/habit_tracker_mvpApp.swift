71d8c685b55b89635d98365e1513d11d10eda2b0
Emirhan İpek <emirrhanipek@gmail.com>
2025-09-15 13:50:25 +0300

    Initial Commit

---
+++ habit-tracker-mvp/habit_tracker_mvpApp.swift
@@ -0,0 +1,17 @@
+//
+//  habit_tracker_mvpApp.swift
+//  habit-tracker-mvp
+//
+//  Created by Emirhan Ipek on 15.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct habit_tracker_mvpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
